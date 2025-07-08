9f2cfa8f0256aa6c140ea61c20558dc5c90509f5
SUBcharacter <farg557@gmail.com>
2025-07-08 14:48:55 +0900

    프로젝트 준비

---
+++ Project/Game/IntegrationLibrary.h
@@ -0,0 +1,12 @@
+#pragma once
+// 필요한 라이브러리 선언
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Config.hpp>
+#include <SFML/System.hpp>
+#include <stdlib.h>
+#include <algorithm>
+#include <map>
+#include <vector>
+
+// 공통적으로 쓰이는 enum, 구조체 선언 공간
