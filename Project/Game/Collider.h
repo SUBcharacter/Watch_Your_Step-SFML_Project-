4602105196fa8372ca0fdf01b7a3063c3f4a4998
SUBcharacter <farg557@gmail.com>
2025-07-19 19:13:17 +0900

    물리 끝 쌕갸

--- Project/Game/Collider.h
+++ Project/Game/Collider.h
@@ -10,8 +10,9 @@ class Collider
 private:
 	Player& player;
 	
-	
 public:
+	Collider(Player& player) : player(player) {}
+
 	bool isColliding(const FloatRect& player, const FloatRect& platform);
 	CollideDir CollidingDirection(const FloatRect& player, const FloatRect& platform);
 	void Collider2D(vector<Platform*> nearPlatform);
